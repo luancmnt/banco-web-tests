32925d71c09f30f9e8070e497ac13e0758e51c3b
Luana Nascimento <luancmnt@36:f6:82:22:0b:f9.home>
2026-06-15 08:44:59 +0100

    Adicionei um teste para validar o login com credenciais inválidas.

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,9 +1,21 @@
 describe('Login', () => {
   it('Login with valid data', () => {
     cy.visit('http://localhost:4000')
+
     cy.get('#username').click().type('julio.lima')
     cy.get('#senha').click().type('123456')
-    cy.get('#login-section > .btn').click()
+    cy.contains('button', 'Entrar').click()
+
     cy.contains('h4', 'Realizar Transferência').should('be.visible')
+  })
+
+   it('Login with invalid data', () => {
+    cy.visit('http://localhost:4000')
+
+    cy.get('#username').click().type('julio.lima')
+    cy.get('#senha').click().type('12345678')
+    cy.contains('button', 'Entrar').click()
+
+    cy.get('.toast').should('have.text', 'Erro no login. Tente novamente.')
   })
 })
